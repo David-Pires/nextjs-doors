03cde6b92fbf89ebd65fdcbbc7c1860a0d459bab
David Pires <developer.david@outlook.com>
2022-02-25 17:56:24 -0300

    Creation of Door Component

--- components/Presente.jsx
+++ components/Presente.jsx
@@ -1,11 +1,16 @@
 import styles from '../styles/Presente.module.css'
 export default function Presente() {
   return (
+
+    {/* Componente Presente */}
   <div className={styles.presente}>   
      <div className={styles.tampa}></div> 
      <div className={styles.corpo}></div> 
      <div className={styles.lacoTampa}></div> 
      <div className={styles.lacoCorpo}></div> 
    </div>
+
+   
+  
   )
 }
