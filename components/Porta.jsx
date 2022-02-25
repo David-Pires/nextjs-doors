62a4bf0767e66d5d2b565f324c242642123d0979
David Pires <developer.david@outlook.com>
2022-02-25 19:23:06 -0300

    positioning of knob

--- components/Porta.jsx
+++ components/Porta.jsx
@@ -3,9 +3,11 @@ import styles from "../styles/Porta.module.css";
 export default function Porta(props) {
   return (
     <div className={styles.area}>
-          <div className={styles.frame}>
+      <div className={styles.frame}>
         <div className={styles.porta}>
           <div className={styles.numero}>3</div>
+          <div className={styles.macaneta}></div>
+
         </div>
       </div>
       <div className={styles.chao}></div>
