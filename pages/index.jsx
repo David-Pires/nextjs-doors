093e31ec502d584a9066968bde02f345433946c7
David Pires <developer.david@outlook.com>
2022-02-25 20:00:55 -0300

    created classes for selected items

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,8 +3,8 @@ import Porta from "../components/Porta";
 export default function Home() {
   return (
     <div style={{display:"flex"}}>
-      <Porta/>
-      <Porta/>
+      <Porta />
+      <Porta selecionada={true }/>
 
     </div>
   );
