26cf15ca9225a68a93d50c54e5b9ad08af157462
David Pires <developer.david@outlook.com>
2022-02-27 18:56:41 -0300

    qtde doors, array of doors [Array.from]

--- model/porta.ts
+++ model/porta.ts
@@ -1,40 +1,59 @@
-export default class PortaModel { 
-  #numero: number
-  #temPresente: boolean
-  #selecionada: boolean
-  #aberta: boolean
+export default class PortaModel {
+  #numero: number;
+  #temPresente: boolean;
+  #selecionada: boolean;
+  #aberta: boolean;
 
-  constructor(numero: number,temPresente = false, selecionada = false, aberta = false) {
-      this.#numero =  numero
-      this.#temPresente = temPresente
-      this.#selecionada = selecionada
-      this.#aberta = aberta
-
+  constructor(
+    numero: number,
+    temPresente = false,
+    selecionada = false,
+    aberta = false
+  ) {
+    this.#numero = numero;
+    this.#temPresente = temPresente;
+    this.#selecionada = selecionada;
+    this.#aberta = aberta;
   }
   get numero() {
-    return this.#numero
+    return this.#numero;
   }
   get temPresente() {
-    return this.#temPresente
+    return this.#temPresente;
   }
   get selecionada() {
-    return this.#selecionada
+    return this.#selecionada;
   }
   get aberta() {
-    return this.#aberta
+    return this.#aberta;
   }
 
   desselecionar() {
-    const selecionada = false
-    return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta)
+    const selecionada = false;
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      selecionada,
+      this.aberta
+    );
   }
 
   alternarSelecao() {
-    const selecionada = !this.selecionada
-    return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta)
+    const selecionada = !this.selecionada;
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      selecionada,
+      this.aberta
+    );
   }
   abrir() {
-    const aberta = true
-    return new PortaModel(this.numero, this.temPresente, this.selecionada, aberta)
+    const aberta = true;
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      this.selecionada,
+      aberta
+    );
   }
- }
+}
