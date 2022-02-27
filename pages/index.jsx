7cf3f45fa4fbd44d46da22a2f4fa8d7bd52337e5
David Pires <developer.david@outlook.com>
2022-02-27 18:12:04 -0300

    Selection mark and unmark Door Component

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,8 +9,8 @@ export default function Home() {
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>  
-    <input type="text" value={texto} onChange={e => setTexto(e.target.value)}/> 
-        <Porta value={p1} /> 
+    <input type="text" value={texto} onChange={e => setTexto(e.target.value)}/>       
+    <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
     </div>
   )
 }
