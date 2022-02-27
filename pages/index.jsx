add815d1fd8fbaedfe50b938240ea2727b1d1d21
David Pires <developer.david@outlook.com>
2022-02-27 19:57:31 -0300

    selection of doors

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,25 @@
 import { useState } from "react";
 import Porta from "../components/Porta.tsx";
+import { atualizarPortas } from '../functions/portas.ts';
 import { criarPortas } from '../functions/portas.ts';
 import PortaModel from "../model/porta.ts";
 
 export default function Home() {
-  const [p1, setP1] = useState(new PortaModel(1))
-  const [p2, setP2] = useState(new PortaModel(2))
-  const [p3, setP3] = useState(new PortaModel(3))
+     const [portas, setPortas] = useState(criarPortas(4, 3))
 
-  
-
-    console.log(criarPortas(3, 2))
+    function renderizarPortas() {
+      return portas.map(porta => {
+        return <Porta key={porta.numero} value={porta} onChange={novaPorta => {
+        setPortas( atualizarPortas(portas, novaPorta))  
+         
+        }} />
+      })
+    }
 
   return (
     <div style={{ display: "flex"}}>             
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
-      <Porta value={p2} onChange={novaPorta => setP2(novaPorta)} />
-      <Porta value={p3} onChange={novaPorta => setP3(novaPorta)} />
-
+      {renderizarPortas()}
     </div>
-  )
-}
+    )
+  }
+
