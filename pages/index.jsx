2e2d460db3a899ce4043d72829c8babc04c7db13
David Pires <developer.david@outlook.com>
2022-02-27 17:41:05 -0300

    treating door as an input type using useState and onChange

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
-import Porta from '../components/Porta.tsx'
-import PortaModel from '../model/porta.ts'
+import { useState } from "react";
+import Porta from "../components/Porta.tsx";
+import PortaModel from "../model/porta.ts";
 
 export default function Home() {
-  const [p1, setP1] = useState(new PortaModel(1))
-  const [p2, setP2] = useState(new PortaModel(2))
+  const [p1, setP1] = useState(new PortaModel(1));
+  const [texto, setText] = useState("...");
+
 
   return (
-    <div style={{display:"flex"}}>
-      <Porta porta={p1}/>
-      <Porta porta={p2}/>
-
-          </div>
-  );
+    <div style={{ display: "flex", flexDirection: "column" }}>  
+    <input type="text" value={texto} onChange={e => setTexto(e.target.value)}/> 
+        <Porta value={p1} /> 
+    </div>
+  )
 }
