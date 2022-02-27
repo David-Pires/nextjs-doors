a2845fb57685ba3340fd3de0f827c16bf3e930bc
David Pires <developer.david@outlook.com>
2022-02-27 00:59:02 -0300

    Typescript configured and classes instantiated

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,16 @@
-import Porta from "../components/Porta";
+import { useState } from 'react';
+import Porta from '../components/Porta.tsx'
+import PortaModel from '../model/porta.ts'
 
 export default function Home() {
+  const [p1, setP1] = useState(new PortaModel(1))
+  const [p2, setP2] = useState(new PortaModel(2))
+
   return (
     <div style={{display:"flex"}}>
-      <Porta />
-      <Porta selecionada={true}/>
-    </div>
+      <Porta porta={p1}/>
+      <Porta porta={p2}/>
+
+          </div>
   );
 }
