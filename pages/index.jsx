a7bd194dc648fd64328231a5cf69c5879a9c25c5
David Pires <developer.david@outlook.com>
2022-02-27 22:03:42 -0300

    new component, typescript reconfigured

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,13 @@
 import { useState } from "react";
-import Porta from "../components/Porta.tsx";
 import { atualizarPortas } from '../functions/portas.ts';
-import { criarPortas } from '../functions/portas.ts';
 import PortaModel from "../model/porta.ts";
 
 export default function Home() {
-     const [portas, setPortas] = useState(criarPortas(4, 3))
-
-    function renderizarPortas() {
-      return portas.map(porta => {
-        return <Porta key={porta.numero} value={porta} onChange={novaPorta => {
-        setPortas( atualizarPortas(portas, novaPorta))  
-         
-        }} />
-      })
+  return (
+   <h1>Inicio de jogo </h1>
+    )
+  
     }
 
-  return (
-    <div style={{ display: "flex"}}>             
-      {renderizarPortas()}
-    </div>
-    )
-  }
+  
 
