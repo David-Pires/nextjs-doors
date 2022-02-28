874372306136055f116bee0c877497afa631736a
David Pires <developer.david@outlook.com>
2022-02-28 20:04:31 -0300

    validação de portas

--- pages/jogo/[portas]/[temPresente].tsx
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -7,8 +7,19 @@ import { useRouter } from 'next/router'
 
 export default function jogo() {
   const router = useRouter()
-
+  
+  const [valido, setValido] = useState(false);
   const [portas, setPortas] = useState([]);
+
+
+  useEffect(() => {
+    const portas = +router.query.portas
+    const temPresente = +router.query.temPresente
+    const qtdePortasValida = portas >= 3 && portas <= 100
+    const temPresenteValido = temPresente >=1 && temPresente <= portas
+    
+    setValido(qtdePortasValida && temPresenteValido)
+}, [portas]) 
 
   useEffect(() => {
       const portas = +router.query.portas
@@ -33,7 +44,10 @@ export default function jogo() {
   return (
     <div id={styles.jogo}>
       <div className={styles.portas}>
-       {renderizarPortas()}
+       {valido ? 
+       renderizarPortas() :
+       <h2>A porta com presente deve estar dentro do número de portas existentes</h2>
+       }
       </div>
       <div className={styles.botoes}>
         <LINK href="/">
